e47dccf610e1f471ee2d8a530359b1be31e29b4d
Harsh <harsh821agarwal@gmail.com>
2022-06-17 00:53:00 +0530

    next.config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
     domains: ['m.media-amazon.com'],
   },
   reactStrictMode: true,
+  env: {
+    MONGO_URI: 'mongodb+srv://coderkart:hC4A2jYjz80j2lV6@cluster0.ybgkzzt.mongodb.net/?retryWrites=true&w=majority',
+    NEXT_PUBLIC_HOST: 'https://coderkart.vercel.app/',
+    NEXT_PUBLIC_KEY: 'rzp_test_e2nmbumusvMsas',
+    KEY_SECRET: 'xZS2I7ZjpcnWnAcnag4u04we',
+    SECRET_KEY_USER: 'mysecret123',
+    JWT_SECRET: 'jwtsecret'
+  },
 }
 
 module.exports = nextConfig
